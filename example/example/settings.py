2f43883a94d0a63172ea5974d1de29826dbf2aa8
Xiang Wang <ramwin@qq.com>
2023-10-31 22:34:44 +0800

    fix: use django_commands in example setting

--- example/example/settings.py
+++ example/example/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "django_commands2",
+    "django_commands",
     "core",
 ]
 
