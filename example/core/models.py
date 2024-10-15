dd63596f33eeebbb36b9048f4168df5c4900e6af
Xiang Wang <ramwin@qq.com>
2024-10-15 21:14:52 +0800

    feat: add core models

--- example/core/models.py
+++ example/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Node(models.Model):
+    parent = models.ForeignKey("self", on_delete=models.CASCADE)
+    note = models.TextField(default="")
+
+
+class Leaf(models.Model):
+    parent = models.ForeignKey(Node, on_delete=models.CASCADE)
